3b89871b53a01014573c9aff01bc4a8c2c60cb4b
Anurup <anurupkoli01@gmail.com>
2023-09-22 20:50:02 +0530

    Stored Data from  Notes to Database

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,53 @@
-const schema = require('../modles/Notes');
-const express = require('express');
+const Notes = require("../modles/Notes");
+const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req,res)=>{
-    res.json();
-})
+// Creating new Notes instsance by "/api/notes/createNotes" request
+router.post(
+  "/createNotes",
+  [
+    body("title", "Enter a valid title with atleast 3 alphabets").isLength({
+      min: 3,
+    }),
+    body(
+      "description",
+      "Enter a valid Descritption with alteastt 10 alphabest"
+    ).isLength({ min: 10 }),
+    body("tag", "Enter a valid tag Ex: General, Student, etc").isString(),
+  ],
+  async (req, res) => {
+    // Checking for any errors w.r.t specified validations
+    let erros = await validationResult(req);
+
+    // if errors are found bad request with error message is sent
+    if (!erros) {
+      res.status(400).json({ erros: erros.message });
+    }
+
+    try {
+      // Trying to check if specified notes is already present in database
+      let notes = await Notes.find({
+        title: req.body.title,
+        description: req.body.description,
+        tag: req.body.tag,
+      });
+      if (notes) {
+        return res.status(400).json({ error: "This file already exists" });
+      }
+
+      // Pushing Notes data to database
+      notes = await Notes.create({
+        title: req.body.title,
+        description: req.body.description,
+        tag: req.body.title,
+      });
+      res.status(200).send(notes);
+    } catch (error) {
+      console.log(error.message);
+      res.status(400).json({ errors: error.message });
+    }
+  }
+);
 
 module.exports = router;
