44500cccd74620d5a35a3711fff0c2d673ad8e8f
Anurup <anurupkoli01@gmail.com>
2023-09-25 19:35:15 +0530

    Frontend Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 
 import './App.css';
+import About from './Components/About';
+import Home from './Components/Home';
 import Navbar from './Components/Navbar';
+import {  
+  BrowserRouter,  
+  Routes,  
+  Route,  
+}   
+from 'react-router-dom';  
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/about' element={<About/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
